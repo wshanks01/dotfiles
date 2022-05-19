e958a5e673987ea4c8a33f2381a444475f9adaf4
Wils Shanks <william.shanks2@gmail.com>
2022-05-19 10:33:34 -0400

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,15 @@
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+export ZSH_CUSTOM="~/.oh-my-zsh/custom"
+
+# spaceship prompt
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+ZSH_THEME="spaceship"
+
+source $ZSH/oh-my-zsh.sh -y
+
+# link dotfiles
+# ln -sf "~/dotfiles/.zshrc" "~/.zshrc"
